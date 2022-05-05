c0a7aab593b07f7fe1ee7c2a6578ec820df6767f
IAAL96 <ivan.anguiano17@hotmail.com>
2022-05-04 19:22:18 -0500

    Started DirFunc and varTable

--- parser.py
+++ parser.py
@@ -3,16 +3,74 @@ import ply.yacc as yacc
 
 tokens = lexer.tokens
 
+functionDir = {}
+currentScope = ""
+currentType = "program"
+variableTable = {}
+'''
+    "global": {
+        "type": "void",
+        "vars": variableTable["global"] -> "i": {
+                                                "type": "int"
+                                                "value": 1
+                                            }
+                                            ...
+    }
+
+    "main": {
+        "type": "void",
+        "vars": variableTable["main"] -> "c": {
+                                              "type": "char"
+                                              "value": "h"
+                                         }
+                                         ...
+    }
+'''
+
 def p_program(t):
-    'program : PROGRAM ID SEMICOLON programVars programFunc main'
+    'program : PROGRAM ID globalTable SEMICOLON programVars programFunc main'
     print("Code OK")
+    print()
+    for i in functionDir:
+        print("\tfunction name: %s" % i)
+        print("\t\ttype: %s" % functionDir[i]["type"])
+        print("\t\tvars: %s" % functionDir[i]["vars"])
+        print()
+    variableTable.clear()
+
+def p_progVT(t):
+    'globalTable : '
+    #currentScope es global por default
+    global currentScope
+    currentScope = "global"
+    #Inicializar variableTable para global y establecer nombre y tipo a program
+    variableTable[currentScope] = {}
+    variableTable[currentScope][t[-1]] = {"type": "program"}
+    #Inicializar functionDir para global scope
+    functionDir[currentScope] = {}
+    #Establecer tipo y vars como referencia a variableTable[global]
+    functionDir[currentScope]["type"] = "void"
+    functionDir[currentScope]["vars"] = variableTable[currentScope]
+    
 
 
 def p_error(t):
    print("Syntax error: Unexpected token '%s' in line %d" % (t.value, t.lexer.lineno))
 
 def p_main(t):
-    'main : MAIN LEFTPAR RIGHTPAR LEFTBRACE statement RIGHTBRACE'
+    'main : mainScope MAIN LEFTPAR RIGHTPAR LEFTBRACE statement RIGHTBRACE'
+
+def p_mainScope(t):
+    'mainScope : '
+    global currentScope
+    variableTable[currentScope]["main"] = {"type": "void"}
+    currentScope = "main"
+    # Inicializar variableTable y functionDir para main scope
+    variableTable[currentScope] = {}
+    functionDir[currentScope] = {}
+    # Establecer tipo de funcion y vars como referencia a variableTable["main"]
+    functionDir[currentScope]["type"] = "void"
+    functionDir[currentScope]["vars"] = variableTable[currentScope]
 
 def p_programVars(t):
     '''programVars : declaration
@@ -37,6 +95,10 @@ def p_PDT(t):
         '''PDT : INT
                  | FLOAT
                  | CHAR '''
+        
+# Al indicar tipo, cambiar currentType por declaracion
+    global currentType
+    currentType = t[1]
 
 def p_return(t):
     'return : RETURN LEFTPAR expression2 RIGHTPAR SEMICOLON'
@@ -61,7 +123,17 @@ def p_while(t):
     'while : WHILE LEFTPAR expression2 RIGHTPAR LEFTBRACE DO statement RIGHTBRACE'
 
 def p_vars(t):
-    'vars : ID varsArray varsComa'
+    'vars : ID addtoTable varsArray varsComa'
+
+def p_addToTable(t):
+    'addToTable : '
+    #Si el ID actual existe en scope, lanzar error
+    if t[-1] in variableTable[currentScope] or t[-1] in variableTable["global"]:
+        print("Error: redefinition of variable '%s' in line %d." % (t[-1], t.lexer.lineno))
+        exit(0)
+    else:
+        # Agrega ID actual a variableTable[scope]
+        variableTable[currentScope][t[-1]] = {"type": currentType}
 
 def p_varsComa(t):
       '''varsComa : COMA vars
@@ -76,15 +148,28 @@ def p_varsArray(t):
                  | '''
 
 def p_function(t):
-     '''function : functionType ID LEFTPAR param RIGHTPAR SEMICOLON LEFTBRACE statement RIGHTBRACE
-                | functionType ID LEFTPAR RIGHTPAR SEMICOLON LEFTBRACE statement RIGHTBRACE '''
+    '''function : functionType ID addToDir LEFTPAR param RIGHTPAR SEMICOLON LEFTBRACE statement RIGHTBRACE
+                | functionType ID addToDir LEFTPAR RIGHTPAR SEMICOLON LEFTBRACE statement RIGHTBRACE '''
+    #Resetear scope a global cuando se salga del scope de la funcion
+    global currentScope
+    currentScope = "global"
 
 def p_param(t):
-    'param: PDT ID functionParam'
+    'param: PDT ID addFuncParams functionParam'
 
 def p_functionParam(t):
       '''functionParam : COMA param
                      | '''
+
+def p_addFuncParams(t):
+    'addFuncParams : '
+    # Si parametro de la funcion ya existe en el scope (o global),dar error
+    if t[-1] in variableTable[currentScope] or t[-1] in variableTable["global"]:
+        print("Error: redefinition of variable '%s' in line %d." % (t[-1], t.lexer.lineno))
+        exit(0)
+    else:
+        # Agregar parametro funcion a variableTable de currentScope
+        variableTable[currentScope][t[-1]] = {"type": currentType}
 
 def p_functionType(t):
      '''functionType : FUNCTION PDT
@@ -95,6 +180,24 @@ def cst_PDT(t):
                 | CST_FLOAT
                 | CST_CHAR '''
 
+def p_addToDir(t):
+    'addToDir : '
+    global currentScope
+    # Si la funcion ya existe en scope global, lanzar error
+    if t[-1] in functionDir["global"] or t[-1] in variableTable["global"]:
+        print("Error: redefinition of '%s' in line %d." % (t[-1], t.lexer.lineno))
+        exit(0)
+    else:
+        # Agregar funcion a variableTable de currentScope
+        variableTable[currentScope][t[-1]] = {"type": currentType}
+        # Cambiar scope a nuevo id de la funcion
+        currentScope = t[-1]
+        # Inicializar variableTable y functionDir para nuevo id de la funcion
+        variableTable[currentScope] = {}
+        functionDir[currentScope] = {}
+        # Establecer nuevo tipo de funcion y vars como referencia a variableTable[currentScope]
+        functionDir[currentScope]["type"] = currentType
+        functionDir[currentScope]["vars"] = variableTable[currentScope]
 
 def p_Expression2(t):
     '''Expression2 : Expression3 Expression22 Expression3
@@ -128,7 +231,11 @@ def p_expFunction(t):
     '''expFunction : PLUS exp
                    | MINUS exp '''
 
-
+def p_setVoid(t):
+    'setVoid : '
+    # Establecer void como currentType
+    global currentType
+    currentType = t[-1]
 
 def p_term(t):
         '''term : factor termFunction
