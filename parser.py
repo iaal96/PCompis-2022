117e526086b46ffd40ae6cea5473df9418eb0cf8
IAAL96 <ivan.anguiano17@hotmail.com>
2022-05-17 16:23:07 -0500

    Added more coments

--- parser.py
+++ parser.py
@@ -30,7 +30,7 @@ def p_program(t):
 	Quadruples.print_all()
 	variableTable.clear()
 
-#Global scope
+#GlobalTable: Inicializar programa y crear variableTable
 def p_globalTable(t):
 	'globalTable : '
 	# Inicializar variableTable para global scope y definir nombre y tipo del programa
@@ -48,6 +48,7 @@ def p_error(t):
 def p_main(t):
 	'main : mainTable MAIN LEFTPAR RIGHTPAR LEFTBRACE declaration statement RIGHTBRACE'
 
+#mainTable: Agregar main a varTable e inicializar propiedades de la funcion main. Actualizar cuadruplo main para saltar al inicio del programa
 def p_mainTable(t):
 	'mainTable : '
 	global currentScope
@@ -72,6 +73,7 @@ def p_programFunc(t):
 	'''programFunc : function programFunc
 				   | '''
 
+#Assignment: Genera cuadruplo en el varTable correspondiente
 def p_assignment(t):
 	'assignment : ID EQUAL Expression2 SEMICOLON'
 	#Si id esta en currentScope, generar cuadruplo y asignar su valor en varTable
@@ -93,6 +95,7 @@ def p_assignment(t):
 	else:
 		Error.undefined_variable(t[1], t.lexer.lineno - 1)
 
+#Declaration: Asignar cuadruplo start para una funcion.
 def p_declaration(t):
 	'declaration : VAR declarationPDT'
 	#Asignar cuadruplo start para funcion
@@ -103,6 +106,7 @@ def p_declarationPDT(t):
 	'''declarationPDT : PDT vars SEMICOLON declarationPDT
 					   | '''
 
+#PDT: Cambiar el currentType de declaracion
 def p_PDT(t):
 	'''PDT : INT
 				 | FLOAT
@@ -118,7 +122,7 @@ def p_return(t):
 def p_if(t):
 	'if : IF LEFTPAR Expression2 RIGHTPAR createJQif THEN LEFTBRACE statement RIGHTBRACE ifElse updateJQ'
 
-#Jump Quad if
+#Jump Quad if: Checar tipo y valor de la expresion y generar cuadruplo para "saltar"
 def p_createJQif(t):
 	'createJQif : '
 	result_type = types.pop()
@@ -135,6 +139,7 @@ def p_createJQif(t):
 	else: 
 		Error.type_mismatch(t[1],t.lexer.lineno)
 
+#Update Jump Quad: Actualiza el cuadruplo con el id del cuadruplo al que debe "saltar"
 def p_updateJQ(t):
 	'updateJQ : '
 	#Actualizar cuadruplos GOTOF
@@ -146,7 +151,7 @@ def p_ifElse(t):
 	'''ifElse : ELSE createJQelse LEFTBRACE statement RIGHTBRACE
 			  | '''
 
-#Jump Quad else
+#Jump Quad else: Crear cuadruplo de "salto" para else
 def p_createJQelse(t):
 	'createJQelse : '
 	#Crear cuadruplo para else
@@ -161,10 +166,12 @@ def p_createJQelse(t):
 def p_for(t):
 	'for : FOR forAssignment TO pushJumpFor Expression2 createQuadFor LEFTBRACE statement RIGHTBRACE updateQuadFor'
 
+#pushJumpFor: Push al id del cuadruplo para "saltar" al stack de saltos.
 def p_pushJumpFor(t):
 	'pushJumpFor : '
 	Quadruples.push_jump(0)
 
+#createQuadFor: Agregar GOTOF a cuadruplos
 def p_createQuadFor(t):
 	'createQuadFor : '
 	result_type = types.pop()
@@ -181,6 +188,7 @@ def p_createQuadFor(t):
 	else: 
 		Error.type_mismatch(t[1],t.lexer.lineno)
 
+#updateQuadFor: Actualizar cuadruplo GOTOF con el id del cuadruplo al cual debe "saltar"
 def p_updateQuadFor(t):
 	'updateQuadFor : '
 	#Actualizar cuadruplo GOTOF cuando termine el for
@@ -191,6 +199,7 @@ def p_updateQuadFor(t):
 	tmp_count = Quadruples.next_id
 	Quadruples.update_jump_quad(tmp_end, tmp_count)
 
+#forAssignment: Agrega iterador a la tabla de constantes y crea una variable iterativa
 def p_forAssignment(t):
 	'forAssignment : ID EQUAL CST_INT'
 	#Checar si el id existe en currentScope y asignar su valor
@@ -207,10 +216,12 @@ def p_forAssignment(t):
 		Error.undefined_variable(t[1], t.lexer.lineno)
 
 
+#pushLoop: Push al id del cuadruplo al stack de "saltos"
 def p_pushLoop(t):
 	'pushLoop : '
 	Quadruples.push_jump(1)
 
+#startLoop: Checar tipo del resultado de la expresion, generar cuadruplo y hacer push al id de salto al stack de salto.
 def p_startLoop(t):
 	'startLoop : '
 	result_type = types.pop()
@@ -229,6 +240,7 @@ def p_startLoop(t):
 	else :
 		Error.type_mismatch(t[1],t.lexer.lineno)
 
+#endLoop: Generar cuadruplo despues de que el estatuto de while termine y actualizar el GOTOF con id al final del cuadruplo loop.
 def p_endLoop(t):
 	'endLoop : '
 	#Generar cuadruplo cuando el while termine y actualizar GOTOF
@@ -249,6 +261,7 @@ def p_while(t):
 def p_vars(t):
 	'vars : ID addVarsToTable varsArray varsComa'
 
+#addVarsToTable: Agrega ID actual (y su tipo) a varTable 
 def p_addVarsToTable(t):
 	'addVarsToTable : '
 	#Si el ID ya existe en el scope o global, dar error
@@ -266,10 +279,12 @@ def p_varsMatrix(t):
 	'''varsMatrix : LEFTBRACK CST_INT RIGHTBRACK
 				  | '''
 
+#varsArray: Declaracion de arreglo
 def p_varsArray(t):
 	'''varsArray : LEFTBRACK CST_INT RIGHTBRACK varsMatrix
 				 | '''
 
+#function: Crea cuadruplo ENDFUNC y define tabla de variables locales.
 def p_function(t):
 	'function : functionType ID addFuncToDir LEFTPAR param RIGHTPAR setParamLength LEFTBRACE declaration statement RIGHTBRACE'
     #Resetear scope a global cuando se salga del scope de la funcion, eliminar varTable y referenciar en functionDir
@@ -292,6 +307,7 @@ def p_functionParam(t):
 	'''functionParam : COMA param
 					 | '''
 
+#addFuncParams: Agrega una lista de tipos de parametros al scope de la funcion.
 def p_addFuncParams(t):
 	'addFuncParams : '
 	# Si parametro de la funcion existe en el scope, dar error
@@ -305,6 +321,7 @@ def p_addFuncParams(t):
 		# Insertar currentTypes en params Queue
 		functionDir[currentScope]["params"].enqueue(currentType)
 
+#setParamLength: Asignar el numero de parametros en la funcion
 def p_setParamLength(t):
 	'setParamLength : '
 	#Asignar el numero de parametro de la funcion al tamano del Queue params
@@ -320,18 +337,22 @@ def p_cst_PDT(t):
 				| CST_CHAR addTypeChar'''
 	t[0] = t[1]
 
+#addTypeInt: Guardar int en tabla de constantes y hacer push al operando al stack de operandos.
 def p_addTypeInt(t):
 	'addTypeInt : '
 	types.push("int")
 
+#addTypeFloat: Guardar float en tabla de constantes y hacer push al operando al stack de operandos.
 def p_addTypeFloat(t):
 	'addTypeFloat : '
 	types.push("float")
 
+#addTypeChar: Guardar char en tabla de constantes y hacer push al operando al stack de operandos.
 def p_addTypeChar(t):
 	'addTypeChar : '
 	types.push("char")
 
+#addFuncToDir: Verifica tipo de funcion e inserta la funcion al directorio de funciones con tipo, varTable y parametros.
 def p_addFuncToDir(t):
 	'addFuncToDir : '
 	# Si la funcion existe en global scope, dar error
@@ -360,7 +381,7 @@ def p_Expression2Nested(t):
     '''Expression2Nested : Expression3 evaluateExp2 Expression22 Expression2Nested
                              | Expression3 evaluateExp2'''
 
-
+#evaluateExp2: Evalua operador y operandos de expresiones booleanas del tipo AND Y or
 def p_evaluateExp2(t):
 	'evaluateExp2 : '
 	global temp
@@ -411,6 +432,7 @@ def p_Expression33(t):
 						 | NOTEQUAL addOperator 
 						 | ISEQUAL addOperator'''
 
+#evaluateExp3: Evalua operador y operandos de expresiones booleanas del tipo >, < , == , y <>.
 def p_evaluateExp3(t):
 	'evaluateExp3 : '
 	global temp
@@ -457,6 +479,7 @@ def p_exp(t):
 	'''exp : term evaluateTerm expFunction
 		   | term evaluateTerm '''
 
+#evaluateTerm: evalua operador y operandos del tipo + y - para variables y variables dimensionadas.
 def p_evaluateTerm(t):
 	'evaluateTerm : '
 	global temp
@@ -496,6 +519,7 @@ def p_expFunction(t):
     '''expFunction : PLUS addOperator exp
                    | MINUS addOperator exp '''
 
+#setVoidType: Define tipo de funcion como Void
 def p_setVoidType(t):
 	'setVoidType : '
 	# Definir void como currentType
@@ -506,6 +530,7 @@ def p_term(t):
         '''term : factor evaluateFactor termFunction
             | factor evaluateFactor'''
 
+#evaluateFactor: Evalua operador y operandos el tipo * y / para variables y variables dimensionadas (multiplicacion)
 def p_evaluateFactor(t):
 	'evaluateFactor : '
 	global temp
@@ -543,6 +568,7 @@ def p_termFunction(t):
 	'''termFunction : MULTIPLY addOperator term
 					| DIVIDE addOperator term '''
 
+#addOperator: Push a operador read al stack de operadores
 def p_addOperator(t):
 	'addOperator : '
 	operators.push(t[-1])
@@ -557,6 +583,7 @@ def p_factor(t):
 	#'addOperand : '
 	#operands.push(t[-1])
 
+#addTypeId: ***
 def p_addTypeId(t):
 	'addTypeId : '
 	#Hacer push a los tipos al stack de tipos
@@ -572,7 +599,7 @@ def p_addOperandCst(t):
 	'addOperandCst : '
 	operands.push(t[-1])
 
-
+#addOperandId: ***
 def p_addOperandId(t):
 	'addOperandId : '
 	#Agregar el valor del operando de currentcope al stack de operandos
@@ -601,6 +628,7 @@ def p_id_listFunction(t):
 	'''id_listFunction : COMA id_list
 					   | '''
 
+#addRead: Genera un cuadruplo read y le hace push a la lista de cuadruplos
 def p_addRead(t):
 	'addRead : '
 	#Genera cuadruplo Read
@@ -620,6 +648,7 @@ def p_printFunction(t):
 def p_printFunction2(t):
 	'printFunction2 : printFunction'
 
+#addPrint: Genera un cuadruplo print y le hace push a la lista de cuadruplos
 def p_addPrint(t):
 	'addPrint : '
 	#Genera cuadruplo print
@@ -632,6 +661,7 @@ def p_print_param(t):
 	'''print_param : Expression2 addPrint
 				   | CST_STRING addPrintString '''
 
+#addPrintString: Lee un string y lo guarda en la tabla de constantes para luego imprimpirlo con el operador PRINT
 def p_addPrintString(t):
 	'addPrintString : '
 	#Agrega string al cuadruplo print
@@ -641,6 +671,7 @@ def p_addPrintString(t):
 def p_module(t):
 	'module : ID checkFunctionExists generateERASize LEFTPAR moduleFunction nullParam RIGHTPAR generateGosub SEMICOLON'
 
+#checkFunctionExists: Verifica que la funcion existe en el directorio de Funciones y le hace push al operador del modulo al stack.
 def p_checkFunctionExists(t):
 	'checkFunctionExists : '
 	if t[-1] not in functionDir:
@@ -648,6 +679,7 @@ def p_checkFunctionExists(t):
 	global funcName
 	funcName = t[-1]
 
+#generateERASize: Crea el cuadruplo ERA con el directorio de la funcion que sera llamada.
 def p_generateERASize(t):
 	'generateERASize : '
 	#Generar tamano ERA pendiente...
@@ -657,6 +689,7 @@ def p_generateERASize(t):
 	global k
 	k = 1
 
+#nullParam: Lanza error si falta un parametro en una llamada de funcion
 def p_nullParam(t):
 	'nullParam : '
 	global k
@@ -664,12 +697,12 @@ def p_nullParam(t):
 	if k < len(functionDir[funcName]["params"].values()):
 		Error.unexpected_number_of_arguments(funcName, t.lexer.lineno)
 
-
+#generateGosub: Crea el cuadruplo Gosub con la direccion de la funcion a llamar **
 def p_generateGosub(t):
 	'generateGosub : '
 	tmp_quad = Quadruple("GOSUB", funcName, "_", functionDir[funcName]["start"])
 	Quadruples.push_quad(tmp_quad)
-
+#generateParam: Crea el cuadruplo PARAM con el opreando que esta siendo leido.
 def p_generateParam(t):
 	'generateParam : '
 	global k
@@ -684,6 +717,7 @@ def p_generateParam(t):
 	else:
 		Error.type_mismatch_module(funcName, t.lexer.lineno)
 
+#nextParam: agrega 1 al iterador de param.
 def p_nextParam(t):
 	'nextParam : '
 	global k
